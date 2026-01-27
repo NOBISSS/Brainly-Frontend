8b596d7f8ab9920c1a17de0fbbfc15dc21bd0ad8
Parth Chauhan <parthchauhan25125@gmail.com>
2026-01-28 01:40:34 +0530

    CHANGED

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 //export const BACKEND_URL="http://localhost:3000/";
-export const BACKEND_URL="https://13.62.231.216/";
-//export const BACKEND_URL="/";
+//export const BACKEND_URL="https://13.62.231.216/";
+export const BACKEND_URL="/";
