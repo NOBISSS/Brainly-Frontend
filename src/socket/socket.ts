6334442c24180aedb250f34eb7b691f72b88ab0d
Parth Chauhan <parthchauhan25125@gmail.com>
2026-05-09 17:42:28 +0530

    Socket Issue Production Fixes

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -1,6 +1,14 @@
-import { BACKEND_URL } from "@/config";
+// src/socket/socket.ts
 import { io } from "socket.io-client";
 
-export const socket = io(BACKEND_URL, {
-  withCredentials: true,
+const SOCKET_URL = location.hostname=="localhost"
+? "http://localhost:3000"
+: "http://13.62.231.216";  // ✅ direct to VPS through Nginx, bypasses Vercel
+
+export const socket = io(SOCKET_URL, {
+    withCredentials: true,
+    transports: ["websocket", "polling"],
+    reconnection: true,
+    reconnectionAttempts: 5,
+    reconnectionDelay: 1000,
 });
